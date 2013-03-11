e85aedd426ec020d0fdf7315774c2e164a70f995
Txus Ordorika <txusinho@gmail.com>
2013-03-11 23:00:48 +0100

    deleted greeting

--- test/uiSpec.js
+++ test/uiSpec.js
@@ -5,7 +5,7 @@ describe("Ui module ", function(){
         foo = myDirector.Ui.Button.createButton("type");
         spyOn(foo, 'constructor');
     });
-console.log('kaixo');
+
     it("creates objects instances of CAATContainer", function(){
         expect(foo).not.toBe(null);
     });
