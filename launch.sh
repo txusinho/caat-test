6fed956f8fba5e637c8ab441dabfbe90d5c16ee4
Txus Ordorika <txusinho@gmail.com>
2013-03-13 00:27:10 +0100

    comment on launch.sh

--- launch.sh
+++ launch.sh
@@ -1,1 +1,10 @@
+#
+# Launches two separate gnome terminals. One with testacular server
+# and the other with livereload plugin started.
+#
+# Works under ubuntu 12.04, gnome-classic theme, and with livereload and
+# testacular installed with default configurations.
+#
+# If somebod y asks for them , I'd commit Guardfiles and Gemfiles
+#
 gnome-terminal --tab -t testacular -e "testacular start test/testacular.conf.js" --tab -e "livereload" -t livereload
